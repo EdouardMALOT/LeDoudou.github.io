7d83cc071a23dfee3e9c0e06a9017f41e153a456
LeDoudou <edouard.malot@gmail.com>
2017-01-23 15:27:57 +0100

    Loading optimisation

--- PSD2HTML/js/main.js
+++ PSD2HTML/js/main.js
@@ -3,24 +3,26 @@ $(document).ready(function(){
 //Init wow lib  for features animation
     new WOW().init();
     
-    
-//Hero1 section animation 
-    //When the content is reashed
-    $('.js-wp-iphone').waypoint(function (direction) {
-         //Animate
-        $('.js-wp-iphone').addClass('animated slideInUp');             
-    }, {
-        offset: '100%'
-    });
-    
-    //Callback when iphone slideInUp is finished
-    $(".js-wp-iphone").bind('oanimationend animationend webkitAnimationEnd', function() { 
-        $(".iphone-btn").delay(200).animate( {top: "+=3"}, 250, function() {
-            // Animation complete.
-            $('.iphone-screen').delay(100).addClass('screen-on');            
+ 
+ window.onload = function() { 
+  //Hero1 section animation
+        //When the content is reashed   
+        $('.js-wp-iphone').waypoint(function (direction) {
+             //Animate
+            $('.js-wp-iphone').addClass('animated slideInUp');             
+        }, {
+            offset: '100%'
         });
-        
-        $(".iphone-btn").delay(300).animate( {top: "+=-3"}, 250);
-    });
 
+        //Callback when iphone slideInUp is finished
+        $(".js-wp-iphone").bind('oanimationend animationend webkitAnimationEnd', function() { 
+            $(".iphone-btn").delay(200).animate( {top: "+=3"}, 250, function() {
+                // Animation complete.
+                $('.iphone-screen').delay(100).addClass('screen-on');            
+            });
+
+            $(".iphone-btn").delay(300).animate( {top: "+=-3"}, 250);
+        });
+  };
+    
 });
