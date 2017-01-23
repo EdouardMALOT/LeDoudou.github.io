962fd7696280fed73ffe7b6453102264617965b0
LeDoudou <edouard.malot@gmail.com>
2017-01-23 15:42:06 +0100

    Loading optimisation 2 (better this time)

--- PSD2HTML/js/main.js
+++ PSD2HTML/js/main.js
@@ -1,19 +1,12 @@
-$(document).ready(function(){ 
- 
-//Init wow lib  for features animation
-    new WOW().init();
+//Wait until all page is dowloaded (img also)
+$(window).on("load",function(){ 
     
- 
- window.onload = function() { 
-  //Hero1 section animation
-        //When the content is reashed   
-        $('.js-wp-iphone').waypoint(function (direction) {
-             //Animate
-            $('.js-wp-iphone').addClass('animated slideInUp');             
-        }, {
-            offset: '100%'
-        });
-
+    //Init wow lib  for features animation
+        new WOW().init();
+    
+    //Hero1 section animation     
+        $('.js-wp-iphone').addClass('animated slideInUp')
+      
         //Callback when iphone slideInUp is finished
         $(".js-wp-iphone").bind('oanimationend animationend webkitAnimationEnd', function() { 
             $(".iphone-btn").delay(200).animate( {top: "+=3"}, 250, function() {
@@ -22,7 +15,5 @@ $(document).ready(function(){
             });
 
             $(".iphone-btn").delay(300).animate( {top: "+=-3"}, 250);
-        });
-  };
-    
+        });  
 });
