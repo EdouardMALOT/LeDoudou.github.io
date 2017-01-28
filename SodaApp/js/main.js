2fe2d4483fe86a2310ecf3c535f69f5e7f55f28c
Edouard <edouard.malot@gmail.com>
2017-01-28 14:12:16 +0100

    Change directory Soda name

---
+++ SodaApp/js/main.js
@@ -0,0 +1,10 @@
+//Wait until all page is dowloaded (img also)
+//-------------------------------------------
+    $(window).on("load",function(){ 
+        
+         //Init wow lib  for features animation
+        new WOW().init();
+    });
+
+
+
